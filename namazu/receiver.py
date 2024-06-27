e8838be972b72398a01562c5e41ab8fee09f8440
Abril-Barrera <abril.barrera.moreno@gmail.com>
2024-06-27 11:53:29 -0600

    .

--- namazu/receiver.py
+++ namazu/receiver.py
@@ -1,15 +1,24 @@
 import board
 import busio
+import time
 
-uart = busio.UART(board.GP0, board.GP1, baudrate=9600, timeout=0.1)
+uart = busio.UART(board.GP0, board.GP1, baudrate=9600)
 
 def receive_message():
-    data = uart.read(32)
+    data = uart.readline()
+
     if data is not None:
-        return data.decode('utf-8').strip()
+        print('data is not none')
+        print(data)
+        decoded = data.decode('utf-8').strip()
+        return decoded
     return None
 
 while True:
+    print('Listening to messasges... ')
     message = receive_message()
+
     if message:
-        print("Received:", message)
+        print('Received: ', message)
+
+    time.sleep(1)
